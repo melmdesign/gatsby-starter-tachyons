40789edf54384599844019cf663bbb5ef8d9b4c6
Liam Murphy <liamm.designs@gmail.com>
2019-02-09 22:38:42 +0100

    Tweak 2 work

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import './custom.css'
 const IndexPage = () => (
   <div>
     <h1 className="fw5 sans-serif">Liam Murphy</h1>
-    <p class="fs-normal">Lead Product Designer @Donut</p>
-    <Link to="/page-2/">View my work.</Link>
+    <p>Lead Product Designer @Donut</p>
+    <Link to="/work/">View my work.</Link>
   </div>
 )
 
