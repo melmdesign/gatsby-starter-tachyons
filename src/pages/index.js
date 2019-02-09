609a47dda0b959493ebdab494dcf1977b3fa687d
Liam Murphy <liamm.designs@gmail.com>
2019-02-09 22:34:29 +0100

    Various Tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from "gatsby"
+import './custom.css'
 
 const IndexPage = () => (
   <div>
     <h1 className="fw5 sans-serif">Liam Murphy</h1>
-    <p>Lead Product Designer @Donut</p>
+    <p class="fs-normal">Lead Product Designer @Donut</p>
     <Link to="/page-2/">View my work.</Link>
   </div>
 )
